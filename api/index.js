e0827a2f1d0be6048be741ee01f843bd1cfeefe6
hasnnaaa <hasnaamalia223@gmail.com>
2025-11-30 13:12:31 +0700

    Fix Vercel limit

--- api/index.js
+++ api/index.js
@@ -1,37 +1,4 @@
-import express from 'express';
-import cors from 'cors';
-import authRoutes from './routes/authRoutes.js';
-import articleRoutes from './routes/articleRoutes.js';
-import bookmarkRoutes from './routes/bookmarkRoutes.js';
-import interactRoutes from './routes/interactRoutes.js';
-import userRoutes from './routes/userRoutes.js';
-
-const app = express();
-const port = 3001;
-
-// Middleware
-app.use(cors());
-app.use(express.json());
-
-// Routes Registration
-app.use('/api/auth', authRoutes);
-app.use('/api/articles', articleRoutes);
-app.use('/api/bookmarks', bookmarkRoutes);
-app.use('/api', interactRoutes); 
-app.use('/api/users', userRoutes);
-
-// Root
-app.get('/', (req, res) => {
-  res.send('ChronoApp API is running... 🚀');
-});
-
-if (process.env.VERCEL) {
-    module.exports = app;
-} else {
-    const port = 3001;
-    app.listen(port, () => {
-        console.log(`🚀 Server Backend jalan di http://localhost:${port}`);
-    });
-}
+// api/index.js
+import app from '../server/index.js';
 
 export default app;
